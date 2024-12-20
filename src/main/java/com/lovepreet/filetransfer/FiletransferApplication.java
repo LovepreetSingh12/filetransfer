d2e9e322942ec2d654aaebc854cf5b5720c2240e
LovepreetSingh12 <luvprits33@gmail.com>
2024-12-20 23:34:52 +0530

    file transfer initial check in

---
+++ src/main/java/com/lovepreet/filetransfer/FiletransferApplication.java
@@ -0,0 +1,13 @@
+package com.lovepreet.filetransfer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FiletransferApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FiletransferApplication.class, args);
+	}
+
+}
